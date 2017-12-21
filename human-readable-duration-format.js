238979f4cc6a043bf3d28d1ce7bd53aeffe3f618
strawbee <yuejiao.hou@gmail.com>
2017-12-21 13:24:11 -0800

    02 - Human Readable Duration Format

---
+++ human-readable-duration-format.js
@@ -0,0 +1,35 @@
+/*
+Human Readable Duration formatDuration
+https://www.codewars.com/kata/52742f58faf5485cae000b9a
+
+In this problem, we create a function that takes in a non-negative integer of seconds and return a string representing the number of seconds in a human readable format. The string takes into account, if applicable, the number of years, days, hours, minutes, and seconds, separated by a comma and a space. The last component is separated by an "and."
+
+Example: formatDuration(3662)  // returns "1 hour, 1 minute and 2 seconds"
+*/
+
+const formatDuration = seconds => {
+  const time = [
+    {name: 'second', number: seconds % 3153600 % 86400 % 3600 % 60},
+    {name: 'minute', number: Math.floor(seconds % 3153600 % 86400 % 3600 / 60)},
+    {name: 'hour', number: Math.floor(seconds % 31536000 % 86400 / 3600)},
+    {name: 'day', number: Math.floor(seconds % 31536000 / 86400)},
+    {name: 'year', number: Math.floor(seconds / 31536000)}
+  ]
+  let arr =[];
+  let s, and, print;
+
+  if (!seconds) return 'now';
+
+  time.forEach(obj => {
+    if (obj.number) {
+      s = (obj.number === 1) ? obj.name : `${obj.name}s`;
+      if (arr.length === 1) and = ' and ';
+      else if (arr.length > 1) and = ', ';
+      else and = '';
+      print = `${obj.number} ${s}${and}`;
+      arr.unshift(print);
+    }
+  });
+
+  return arr.join('');
+}
