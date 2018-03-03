8466db91208ef915f6136b4a5b9d660787f9a003
strawbee <yuejiao.hou@gmail.com>
2018-03-02 18:01:03 -0800

    13 - Mar 02, 2018

--- trailing-zeros-of-n.js
+++ trailing-zeros-of-n.js
@@ -4,6 +4,7 @@ https://www.codewars.com/kata/52f787eb172a8b4ae1000a34
 
 Write a program that will calculate the number of trailing zeros in a factorial of a given number n, without calculating the factorial.
 */
+
 const zeros = n => {
     let num = 5, res = 0;
     while (num < n) {
