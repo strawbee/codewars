90ed2ab76ecc4a148e3d09350199b3bfa31a7e00
strawbee <yuejiao.hou@gmail.com>
2018-03-29 01:51:32 -0700

    23 - Mar 29, 2018

--- ip-validation.js
+++ ip-validation.js
@@ -1,5 +1,5 @@
 /*
-IP Validation
+IP Validation 
 https://www.codewars.com/kata/ip-validation
 
 Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0..255 (included).
