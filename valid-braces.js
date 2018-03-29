6e107450bd98f36ad8b76b31fbb9b829eefe9161
strawbee <yuejiao.hou@gmail.com>
2018-03-29 01:51:15 -0700

    22 - Mar 29, 2018

--- valid-braces.js
+++ valid-braces.js
@@ -1,5 +1,5 @@
 /*
-Valid Braces
+Valid Braces 
 https://www.codewars.com/kata/valid-braces
 
 Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
